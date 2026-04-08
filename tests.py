5e8ba8264d8f6be7d76e5d8af1932e05b14b060c
«Pavel <pakoryagin@ya.ru>
2026-04-08 12:28:46 +0800

    срединное число

--- tests.py
+++ tests.py
@@ -576,4 +576,13 @@ print('Hi!')
 # else:
 #     print('Разносторонни')
 
+a = int(input())
+b = int(input())
+c = int(input())
+if  (b <= a <= c) or (c <= a <= b):
+    print(a)
+elif (a <= b <= c) or (c <= b <= a):
+    print(b)
+else:
+    print(c)
 
